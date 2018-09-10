b2542c0d09cd8a4d8871d472e494e3359c2d20a6
shoshi <shoshiroberts@gmail.com>
2018-09-10 12:04:26 -0400

    New nuxt app from fftf/nuxt-starter-template

---
+++ assets/js/helpers.js
@@ -0,0 +1,108 @@
+// Global Helpers
+//
+// Usage in a Vue component:
+// import { createMetaTags, myOtherHelperFunction } from '~/assets/js/helpers'
+
+
+// Add meta tags per page for Twitter and Facebook share cards
+export function createMetaTags(tags={}) {
+  const meta = {}
+
+  tags = Object.assign({
+    'author': 'Fight for the Future',
+    'og:description': tags.description,
+    'og:image': tags.image,
+    'og:site_name': 'TODO: site name',
+    'og:title': tags.title,
+    'og:type': 'website',
+    'og:url': tags.url,
+    'twitter:card': 'summary_large_image',
+    'twitter:description': tags.description,
+    'twitter:image': tags.image,
+    'twitter:site': '@FightForTheFtr',
+    'twitter:title': tags.title,
+    'twitter:url': tags.url
+  }, tags)
+
+  const fakeTagNames = ['title', 'image', 'url']
+
+  for (let key of Object.keys(tags)) {
+    if (key.match(/^og\:/)) {
+      meta[key] = {
+        hid: key,
+        property: key,
+        content: tags[key]
+      }
+    }
+    else if (!fakeTagNames.includes(key)) {
+      meta[key] = {
+        hid: key,
+        name: key,
+        content: tags[key]
+      }
+    }
+  }
+
+  return Object.values(meta)
+}
+
+// Open a pop-up window (mostly for sharing actions)
+export function openPopup(url, title='popup', w=600, h=500) {
+  // Fixes dual-screen position
+  var dualScreenLeft = window.screenLeft != undefined ? window.screenLeft : screen.left;
+  var dualScreenTop = window.screenTop != undefined ? window.screenTop : screen.top;
+
+  var width = window.innerWidth ? window.innerWidth : document.documentElement.clientWidth ? document.documentElement.clientWidth : screen.width;
+  var height = window.innerHeight ? window.innerHeight : document.documentElement.clientHeight ? document.documentElement.clientHeight : screen.height;
+
+  var left = ((width / 2) - (w / 2)) + dualScreenLeft;
+  var top = ((height / 2) - (h / 2)) + dualScreenTop;
+  var newWindow = window.open(url, title, 'scrollbars=yes, width=' + w + ', height=' + h + ', top=' + top + ', left=' + left);
+
+  // Puts focus on the newWindow
+  if (window.focus) {
+    newWindow.focus();
+  }
+}
+
+/**
+ * Smooth scroll animation
+ * @param {int} endX: destination x coordinate
+ * @param {int) endY: destination y coordinate
+ * @param {int} duration: animation duration in ms
+ */
+export function smoothScrollTo(endX, endY, duration) {
+  var startX = window.scrollX || window.pageXOffset,
+    startY = window.scrollY || window.pageYOffset,
+    distanceX = endX - startX,
+    distanceY = endY - startY,
+    startTime = new Date().getTime()
+
+  duration = typeof duration !== 'undefined' ? duration : 400
+
+  // Easing function
+  var easeInOutQuart = function(time, from, distance, duration) {
+    if ((time /= duration / 2) < 1) return distance / 2 * time * time * time * time + from
+    return -distance / 2 * ((time -= 2) * time * time * time - 2) + from
+  }
+
+  var timer = window.setInterval(function() {
+    var time = new Date().getTime() - startTime,
+      newX = easeInOutQuart(time, startX, distanceX, duration),
+      newY = easeInOutQuart(time, startY, distanceY, duration)
+    if (time >= duration) {
+      window.clearInterval(timer)
+    }
+    window.scrollTo(newX, newY)
+  }, 1000 / 60) // 60 fps
+}
+
+// Smooth scroll animation to an element by ID
+export function smoothScrollToElement(el, duration) {
+  duration = typeof duration !== 'undefined' ? duration : 500;
+  el = typeof el === 'string' ? document.querySelector(el) : el;
+
+  if (el) {
+    smoothScrollTo(el.offsetLeft, el.offsetTop, duration)
+  }
+}
---
+++ pages/index.vue
@@ -0,0 +1,118 @@
+<template>
+  <div>
+    <section class="sml-pad-y3 med-pad-y6">
+      <div class="wrapper">
+        <div class="row">
+          <div class="sml-c12 lrg-c8 grid-center text-center">
+            <h1>Your title goes here</h1>
+            <p class="sml-push-y2 med-push-y3">
+              Sub heading goes here, lorem ipsum dolor sit amet, consectetur
+              adipiscing elit. In nibh libero, venenatis sed justo eu,
+              sollicitudin sollicitudin nisi. Integer semper tortor orci,
+              id ultricies velit laoreet in. Vestibulum sit amet ante vel risus
+              ornare ultrices sed id leo.
+            </p>
+            <a class="btn btn-block sml-push-y2 med-push-y3" href="#TODO">
+              Call to action
+            </a>
+
+            <ul class="hoz sml-push-y2 med-push-y3">
+              <li>
+                <a href="#TODO-section-1"
+                   @click.prevent="scrollTo('#TODO-section-1')">
+                  Link to section 1
+                </a>
+              </li>
+              <li>
+                <a href="#TODO-section-2"
+                   @click.prevent="scrollTo('#TODO-section-2')">
+                  Link to section 2
+                </a>
+              </li>
+            </ul>
+          </div> <!-- .c -->
+        </div> <!-- .row -->
+      </div> <!-- .wrapper -->
+    </section>
+
+    <section id="TODO-section-1" class="sml-pad-y3 med-pad-y6 fill-grey-light">
+      <div class="wrapper">
+        <div class="row">
+          <div class="sml-c12 lrg-c8 grid-center text-center">
+            <h2>Section title goes here</h2>
+            <p class="sml-push-y2 med-push-y3">
+              Body text goes here, lorem ipsum dolor sit amet, consectetur
+              adipiscing elit. In nibh libero, venenatis sed justo eu,
+              sollicitudin sollicitudin nisi. Integer semper tortor orci,
+              id ultricies velit laoreet in. Vestibulum sit amet ante vel risus
+              ornare ultrices sed id leo.
+            </p>
+            <p>
+              Body text goes here, lorem ipsum dolor sit amet, consectetur
+              adipiscing elit. In nibh libero, venenatis sed justo eu,
+              sollicitudin sollicitudin nisi. Integer semper tortor orci,
+              id ultricies velit laoreet in. Vestibulum sit amet ante vel risus
+              ornare ultrices sed id leo.
+            </p>
+          </div> <!-- .c -->
+        </div> <!-- .row -->
+      </div> <!-- .wrapper -->
+    </section>
+
+    <section id="TODO-section-2" class="sml-pad-y3 med-pad-y6">
+      <div class="wrapper">
+        <div class="row">
+          <div class="sml-c12 lrg-c8 grid-center text-center">
+            <h2>Section title goes here</h2>
+            <p class="sml-push-y2 med-push-y3">
+              Body text goes here, lorem ipsum dolor sit amet, consectetur
+              adipiscing elit. In nibh libero, venenatis sed justo eu,
+              sollicitudin sollicitudin nisi. Integer semper tortor orci,
+              id ultricies velit laoreet in. Vestibulum sit amet ante vel risus
+              ornare ultrices sed id leo.
+            </p>
+          </div> <!-- .c -->
+        </div> <!-- .row -->
+      </div> <!-- .wrapper -->
+    </section>
+
+    <SocialSidebar />
+  </div>
+</template>
+
+<script>
+import config from '~/config'
+import { createMetaTags, smoothScrollToElement } from '~/assets/js/helpers'
+import SocialSidebar from '~/components/SocialSidebar'
+
+export default {
+  components: {
+    SocialSidebar
+  },
+
+  head() {
+    return {
+      title: config.sharing.title,
+      meta: createMetaTags({
+        title: config.sharing.title,
+        description: config.sharing.description,
+        image: config.sharing.image,
+        url: config.sharing.url
+      })
+    }
+  },
+
+  methods: {
+    scrollTo(hash) {
+      const duration = 500
+      smoothScrollToElement(hash, duration)
+      // WARNING: Since there is no server a setTimeout is ok. However, with a
+      // server this is a dangerous eval. Remove if this project ever is hosted
+      // with a JS server.
+      setTimeout(() => {
+        location.hash = hash
+      }, duration)
+    }
+  }
+}
+</script>

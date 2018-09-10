a620ba5ed0a6d1ac87e2b62b4b9719c03c646565
shoshi <shoshiroberts@gmail.com>
2018-09-10 12:48:11 -0400

    Add initial content from google doc

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,7 +3,7 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: 'vote-for-net-neutrality',
+    title: 'Vote for Net Neutrality',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
--- pages/index.vue
+++ pages/index.vue
@@ -1,76 +1,224 @@
 <template>
   <div>
-    <section class="sml-pad-y3 med-pad-y6">
+    <section id="text-me" class="sml-pad-y3 med-pad-y6">
       <div class="wrapper">
         <div class="row">
           <div class="sml-c12 lrg-c8 grid-center text-center">
-            <h1>Your title goes here</h1>
+            <h1>Vote for Net Neutrality</h1>
             <p class="sml-push-y2 med-push-y3">
-              Sub heading goes here, lorem ipsum dolor sit amet, consectetur
-              adipiscing elit. In nibh libero, venenatis sed justo eu,
-              sollicitudin sollicitudin nisi. Integer semper tortor orci,
-              id ultricies velit laoreet in. Vestibulum sit amet ante vel risus
-              ornare ultrices sed id leo.
+              TODO: widget
             </p>
             <a class="btn btn-block sml-push-y2 med-push-y3" href="#TODO">
-              Call to action
+              Text me
             </a>
-
-            <ul class="hoz sml-push-y2 med-push-y3">
-              <li>
-                <a href="#TODO-section-1"
-                   @click.prevent="scrollTo('#TODO-section-1')">
-                  Link to section 1
-                </a>
-              </li>
-              <li>
-                <a href="#TODO-section-2"
-                   @click.prevent="scrollTo('#TODO-section-2')">
-                  Link to section 2
-                </a>
-              </li>
-            </ul>
           </div> <!-- .c -->
         </div> <!-- .row -->
       </div> <!-- .wrapper -->
     </section>
 
-    <section id="TODO-section-1" class="sml-pad-y3 med-pad-y6 fill-grey-light">
+    <section id="how" class="sml-pad-y3 med-pad-y6 fill-grey-light">
       <div class="wrapper">
         <div class="row">
           <div class="sml-c12 lrg-c8 grid-center text-center">
-            <h2>Section title goes here</h2>
+            <h2>How does it work?</h2>
             <p class="sml-push-y2 med-push-y3">
-              Body text goes here, lorem ipsum dolor sit amet, consectetur
-              adipiscing elit. In nibh libero, venenatis sed justo eu,
-              sollicitudin sollicitudin nisi. Integer semper tortor orci,
-              id ultricies velit laoreet in. Vestibulum sit amet ante vel risus
-              ornare ultrices sed id leo.
+              Simply enter your number above or text HELLO to (XXX) XXX-XXXX and
+              respond to a few questions&hellip; it only takes a minute to
+              complete! Once you&rsquo;re done, we&rsquo;ll help you register to
+              vote and give you everything you need to get to the polls. In
+              several states, your answers submit directly to state online forms
+              for voter registration, so there are no paper forms to fill out.
+              In other states, we send you a pre-filled form by email or — if
+              you don&rsquo;t have a printer — by mail. We include a stamped
+              envelope, so to complete your registration you just have to sign
+              and mail it.
             </p>
             <p>
-              Body text goes here, lorem ipsum dolor sit amet, consectetur
-              adipiscing elit. In nibh libero, venenatis sed justo eu,
-              sollicitudin sollicitudin nisi. Integer semper tortor orci,
-              id ultricies velit laoreet in. Vestibulum sit amet ante vel risus
-              ornare ultrices sed id leo.
+              We also provide links to your state&rsquo;s own online
+              registration system, if they have one. And as election day
+              approaches, we send you all the information you need to vote,
+              including your polling location, early voting info, election day
+              reminders including a handy calendar invite, voter ID requirements
+              &amp; more.
+            </p>
+            <p>
+              Before submitting your registration, we&rsquo;ll give you the
+              chance to verify that your information is correct. Do not provide
+              false information, as your state may consider that a criminal
+              offense. When you&rsquo;re done, we will send you your polling
+              location, early voting info, an &ldquo;I&rsquo;m voting&rdquo;
+              selfie badge, election day reminders (including a handy calendar
+              invite), voter ID requirements, streamlined ways to register to
+              vote &amp; more.
+            </p>
+            <p>
+              And don&rsquo;t worry. We won&rsquo;t share your phone number with
+              anyone, ever. You can read about our privacy policy or check out
+              our terms of service. Remember that you&rsquo;re texting, so data
+              &amp; message rates may apply. You can text STOP any time to stop
+              receiving messages.
+            </p>
+            <a class="btn btn-block sml-push-y2 med-push-y3"
+               @click.prevent="scrollTo('#TODO-section-2')">
+              Text Me!
+            </a>
+            <a class="btn btn-block sml-push-y2 med-push-y3"
+               href="https://m.me/hellovote"
+               target="_blank">
+              Sign up with Messenger
+            </a>
+          </div> <!-- .c -->
+        </div> <!-- .row -->
+      </div> <!-- .wrapper -->
+    </section>
+
+    <section id="widget" class="sml-pad-y3 med-pad-y6">
+      <div class="wrapper">
+        <div class="row">
+          <div class="sml-c12 lrg-c8 grid-center text-center">
+            <h2>Widgets</h2>
+            <p class="sml-push-y2 med-push-y3">
+              Add our widget to your site. It&rsquo;s a small (but powerful!)
+              notice to your site&rsquo;s users that something BIG is happening
+              with net neutrality.
+            </p>
+            <a class="btn btn-block sml-push-y2 med-push-y3" href="#TODO">
+              Try it out
+            </a>
+            <p class="sml-push-y2 med-push-y3">
+              Pretty cool, right? Simply add this line of JavaScript to any page
+              on your site to join the RED ALERT campaign:
+            </p>
+            <p>
+              TODO: url
+            </p>
+            <p>
+              The widget will appear on your page just like you see it here:
+            </p>
+            <p>
+              TODO: Screenshot
+            </p>
+            <p>
+              If you&rsquo;d like to configure the appearance or change the
+              location of the widget on your web pages, just
+              <a href="#TODO" target="_blank">visit our GitHub page</a> to learn
+              how.
+            </p>
+            <p>
+              Please feel free to create your own custom way to drive awareness
+              of this important issue and encourage your users to contact
+              Congress. But whatever you do, make sure it&rsquo;s epic. THIS is
+              the moment to fight!
             </p>
           </div> <!-- .c -->
         </div> <!-- .row -->
       </div> <!-- .wrapper -->
     </section>
 
-    <section id="TODO-section-2" class="sml-pad-y3 med-pad-y6">
+    <section id="banner-ads" class="sml-pad-y3 med-pad-y6 fill-grey-light">
       <div class="wrapper">
         <div class="row">
           <div class="sml-c12 lrg-c8 grid-center text-center">
-            <h2>Section title goes here</h2>
+            <h2>Post banner ads on your site</h2>
             <p class="sml-push-y2 med-push-y3">
-              Body text goes here, lorem ipsum dolor sit amet, consectetur
-              adipiscing elit. In nibh libero, venenatis sed justo eu,
-              sollicitudin sollicitudin nisi. Integer semper tortor orci,
-              id ultricies velit laoreet in. Vestibulum sit amet ante vel risus
-              ornare ultrices sed id leo.
+              Do you run a site or blog? Use the banner ads below to get the
+              word out on net neutrality. Or, if you&rsquo;re feeling creative,
+              make something yourself and submit it for other sites to use by
+              emailing us at
+              <a href="mailto:team@fightforthefuture.org">
+                team@fightforthefuture.org</a>.
             </p>
+            <p>
+              TODO: Banner Ad Images
+            </p>
+          </div> <!-- .c -->
+        </div> <!-- .row -->
+      </div> <!-- .wrapper -->
+    </section>
+
+    <section id="social" class="sml-pad-y3 med-pad-y6">
+      <div class="wrapper">
+        <div class="row">
+          <div class="sml-c12 lrg-c8 grid-center text-center">
+            <h2>Social media</h2>
+            <p class="sml-push-y2 med-push-y3">
+              If you&rsquo;re active on social media, have a video channel, or
+              moderate an online forum, put your small piece of the Internet on
+              alert by changing your profile on social media to the images
+              below. You can also schedule frequent posts linking to
+              <a href="https://www.battleforthenet.com/" target="_blank">
+                BattleForThe.Net</a>
+              where people can take action.
+              <a href="#TODO" target="_blank">Click here</a>to see ideas for
+              blog posts, emails, tweets and more.
+            </p>
+            <p>
+              <strong>Avatar Images</strong>
+            </p>
+            <p>
+              TODO: Avatar Images
+            </p>
+            <p>
+              <strong>Facebook Header Images</strong>
+            </p>
+            <p>
+              TODO: Facebook Header Images
+            </p>
+            <p>
+              <strong>Twitter Header Images</strong>
+            </p>
+            <p>
+              TODO: Twitter Header Images
+            </p>
+            <p>
+              <strong>Instagram Story</strong>
+            </p>
+            <p>
+              TODO: Instagram Story Images
+            </p>
+          </div> <!-- .c -->
+        </div> <!-- .row -->
+      </div> <!-- .wrapper -->
+    </section>
+
+    <section id="participants" class="sml-pad-y3 med-pad-y6 fill-grey-light">
+      <div class="wrapper">
+        <div class="row">
+          <div class="sml-c12 lrg-c8 grid-center text-center">
+            <h2>Participants</h2>
+            <p class="sml-push-y2 med-push-y3">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
+              suscipit odio sapien, vitae suscipit nibh accumsan quis. Lorem
+              ipsum dolor sit amet, consectetur adipiscing elit. Orci varius
+              natoque penatibus et magnis dis parturient montes, nascetur
+              ridiculus mus.
+            </p>
+            <p>
+              TODO: Participant logos, or remove section
+            </p>
+          </div> <!-- .c -->
+        </div> <!-- .row -->
+      </div> <!-- .wrapper -->
+    </section>
+
+    <section id="scoreboard" class="sml-pad-y3 med-pad-y6">
+      <div class="wrapper">
+        <div class="row">
+          <div class="sml-c12 lrg-c8 grid-center text-center">
+            <h2>Find out where your lawmakers stand</h2>
+            <p class="sml-push-y2 med-push-y3">
+              We&rsquo;re keeping track of who supports net neutrality&hellip;
+              and who doesn&rsquo;t. Now you can, too. Below you'll find a link
+              to a list of your state&rsquo;s Senators and Representatives.
+              Check out whether or not your lawmakers have signed the CRA to
+              support net neutrality protections. If not, let them know how
+              important this issue is to you.
+            </p>
+            <a class="btn btn-block sml-push-y2 med-push-y3"
+               href="https://www.battleforthenet.com/scoreboard/"
+               target="_blank">
+              View the scoreboard
+            </a>
           </div> <!-- .c -->
         </div> <!-- .row -->
       </div> <!-- .wrapper -->

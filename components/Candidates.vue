d53cb21f2da2464c9a0ea8879e4f5578083edd6b
shoshi <shoshiroberts@gmail.com>
2018-09-17 16:46:25 -0400

    Fetch and display candidates

---
+++ components/Candidates.vue
@@ -0,0 +1,26 @@
+<template>
+  <div v-if="race" class="sml-push-y2">
+    <h3>{{ title }}</h3>
+    <ul>
+      <li v-for="candidate in race.candidates" :key="candidate.id">
+        {{ candidate.name }}
+        <span v-if="candidate.incumbent">(Incumbent)</span>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true
+    },
+    race: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
--- components/Scoreboard.vue
+++ components/Scoreboard.vue
@@ -17,9 +17,7 @@
                      class="sml-flex-2 med-flex-3"
                      placeholder="Street Address, City, State ZIP"
                      ref="addressInput" />
-              <button class="btn "
-                 href="https://www.battleforthenet.com/scoreboard/"
-                 target="_blank">
+              <button class="btn" :disabled="isLoading">
                 Search
               </button>
             </form>
@@ -28,13 +26,16 @@
       </div> <!-- .wrapper -->
     </section>
 
-    <section v-if="false"
+    <section v-if="results"
              id="results"
              class="sml-pad-y2 med-pad-y4 fill-brand-dark-color">
       <div class="wrapper">
         <div class="row">
           <div class="sml-c12 lrg-c8 grid-center text-center">
             <h2>Results</h2>
+
+            <Candidates :race="results.senate" title="Senate" />
+            <Candidates :race="results.house" title="House" />
           </div> <!-- .c -->
         </div> <!-- .row -->
       </div> <!-- .wrapper -->
@@ -53,10 +54,19 @@
 </template>
 
 <script>
+import axios from 'axios'
+import Candidates from '~/components/Candidates'
+
 export default {
+  components: {
+    Candidates
+  },
+
   data () {
     return {
-      address: null
+      isLoading: false,
+      address: null,
+      results: null
     }
   },
 
@@ -80,9 +90,12 @@ export default {
       })
     },
 
-    fetchCandidates() {
-      // TODO: fetch candidates
-      console.log('fetchCandidates for:', this.address)
+    async fetchCandidates() {
+      this.results = null
+      this.isLoading = true
+      const { data } = await axios.get(`https://vfnn-scoreboard-api.fftf.xyz/?address=${this.address}`)
+      this.results = data
+      this.isLoading = false
     }
   }
 }

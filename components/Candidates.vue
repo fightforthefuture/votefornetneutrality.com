d137ac204f1ca95750713b6864ca964498c14eab
shoshi <shoshiroberts@gmail.com>
2018-09-18 11:58:06 -0400

    Styling candidates more

--- components/Candidates.vue
+++ components/Candidates.vue
@@ -1,27 +1,40 @@
 <template>
   <div v-if="race" class="sml-push-y2">
     <h3 class="text-center">{{ title }}</h3>
-    <ul class="flex-row sml-push-y2">
-      <li v-for="candidate in race.candidates" :key="candidate.id">
-        <div class="sml-pad-1 is-rounded"
+    <div class="row">
+      <div v-for="candidate in race.candidates" :key="candidate.id"
+           class="sml-c12 med-c6 sml-push-y2">
+        <div class="sml-pad-1 is-rounded candidate fill-brand-med"
              :class="{
-                'fill-success': candidate.supporter,
-                'fill-warn': !candidate.supporter
+                'fill-success': candidate.supporter === true,
+                'fill-warn': candidate.supporter === false
              }">
-          <div class="sml-pad-1 is-rounded fill-brand-darkest">
-            <!-- TODO: better styling -->
-            <span :class="{
-                'text-success': candidate.supporter,
-                'text-warn': !candidate.supporter
-             }">
+          <div class="sml-pad-1 is-rounded fill-brand-darkest candidate-content">
+            <h4 :class="{
+                    'text-success': candidate.supporter === true,
+                    'text-warn': candidate.supporter === false
+                 }">
               {{ candidate.name }}
-            </span><br>
-            {{ candidate.party }}<br>
-            <span v-if="candidate.incumbent">(Incumbent)</span>
-          </div> <!-- .pad -->
-        </div> <!-- .pad -->
-      </li>
-    </ul>
+            </h4>
+            <h6 :class="{ 'text-brand-med': candidate.supporter === null }">
+              {{ candidate.party }}
+            </h6>
+            <h6 v-if="candidate.incumbent" class="tag">Incumbent</h6>
+          </div> <!-- .candidate-content -->
+          <div class="vote-tag fill-brand-med"
+               :class="{
+                  'fill-success': candidate.supporter === true,
+                  'fill-warn': candidate.supporter === false
+               }">
+            <h6 class="text-brand-darkest">
+              <span v-if="candidate.supporter === true">Supports</span>
+              <span v-else-if="candidate.supporter === false">Opposes</span>
+              <span v-else>No Stance</span>
+            </h6>
+          </div> <!-- .vote-tag -->
+        </div> <!-- .candidate -->
+      </div> <!-- .c -->
+    </div> <!-- .row -->
   </div>
 </template>
 

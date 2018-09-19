091dab73daad2e8d8642e7c4c6878c5888e119e5
Ken Mickles <ken@37i.net>
2018-09-19 09:49:50 -0400

    Create article layout for legal pages

---
+++ layouts/article.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <section class="sml-pad-y2 med-pad-y4 text-center">
+      <div class="wrapper">
+        <div class="row">
+          <div class="sml-c12 lrg-c8 grid-center article text-center">
+            <Logo/>
+            <nuxt/>
+          </div>
+        </div>
+      </div>
+    </section>
+    <PageFooter/>
+  </div>
+</template>
+
+<script>
+import Logo from '~/components/Logo'
+import PageFooter from '~/components/PageFooter'
+
+export default {
+  components: {
+    Logo,
+    PageFooter
+  }
+}
+</script>
--- pages/privacy.vue
+++ pages/privacy.vue
@@ -1,122 +1,79 @@
 <template>
   <div>
-    <section class="sml-pad-y2 med-pad-y4">
-      <div class="wrapper">
-        <div class="row">
-          <div class="sml-c12 lrg-c8 grid-center article">
-            <Logo/>
+    <h1>Privacy Policy</h1>
+    <p>
+      Fight for the Future and Fight for the Future Education Fund (collectively, the “HelloVote Providers”) own and operate the HelloVote website and voter registration tool (collectively, the “HelloVote Tool”). This Privacy Policy governs the manner in which the HelloVote Providers collect, use, maintain and disclose information collected from users (“You” or “Users”) of the HelloVote Tool.
+    </p>
 
-            <h1 id="privacy">Privacy Policy</h1>
-            <p>
-              Fight for the Future and Fight for the Future Education Fund (collectively, the “HelloVote Providers”) own and operate the HelloVote website and voter registration tool (collectively, the “HelloVote Tool”). This Privacy Policy governs the manner in which the HelloVote Providers collect, use, maintain and disclose information collected from users (“You” or “Users”) of the HelloVote Tool.
-            </p>
+    <h2>The Types of Information the HelloVote Providers Collect</h2>
+    <h3>Non-personally identifiable information</h3>
+    <p>
+      The HelloVote Providers collect non-personally identifiable information about you when you visit the HelloVote Tool. Such information may include the Internet Protocol (IP) address of the computer you used, the type of browser and operating system you used, the date and time you visited the Tool, the Internet address of the site from which you linked to the HelloVote Tool, and which pages you viewed on the Tool.
+    </p>
 
-            <h2>The Types of Information the HelloVote Providers Collect</h2>
-            <h3>Non-personally identifiable information</h3>
-            <p>
-              The HelloVote Providers collect non-personally identifiable information about you when you visit the HelloVote Tool. Such information may include the Internet Protocol (IP) address of the computer you used, the type of browser and operating system you used, the date and time you visited the Tool, the Internet address of the site from which you linked to the HelloVote Tool, and which pages you viewed on the Tool.
-            </p>
+    <h3>Personally identifiable information</h3>
+    <p>
+      The HelloVote Providers collect personally identifiable information, such as names, cell phone numbers, mailing addresses, dates of birth, party affiliation, social security numbers, state identification numbers, etc., from Users when they voluntarily provide such information to the HelloVote Providers in connection with using the HelloVote Tool.
+    </p>
 
-            <h3>Personally identifiable information</h3>
-            <p>
-              The HelloVote Providers collect personally identifiable information, such as names, cell phone numbers, mailing addresses, dates of birth, party affiliation, social security numbers, state identification numbers, etc., from Users when they voluntarily provide such information to the HelloVote Providers in connection with using the HelloVote Tool.
-            </p>
+    <h2>How the HelloVote Providers Use the Information that They Collect</h2>
+    <p>
+      The HelloVote Providers use Users’ personal information to help Users register to vote. The HelloVote Providers will also add you to their distribution lists and may contact you via mail, email and/or text message with information relating to your voter registration, the 2016 general election, as well as other information that the HelloVote Providers think will be of interest to you. If at any time you wish to stop receiving communications from the HelloVote Providers, please follow the unsubscribe instructions included on all such communications from the HelloVote Providers. By providing your cell phone number to the HelloVote Providers, you consent to be contacted at that number on any subject from the HelloVote Providers, unless and until you request that such messages stop. Message and data rates may apply.
+    </p>
 
-            <h2>How the HelloVote Providers Use the Information that They Collect</h2>
-            <p>
-              The HelloVote Providers use Users’ personal information to help Users register to vote. The HelloVote Providers will also add you to their distribution lists and may contact you via mail, email and/or text message with information relating to your voter registration, the 2016 general election, as well as other information that the HelloVote Providers think will be of interest to you. If at any time you wish to stop receiving communications from the HelloVote Providers, please follow the unsubscribe instructions included on all such communications from the HelloVote Providers. By providing your cell phone number to the HelloVote Providers, you consent to be contacted at that number on any subject from the HelloVote Providers, unless and until you request that such messages stop. Message and data rates may apply.
-            </p>
+    <h2>How the HelloVote Providers Protect User Information</h2>
+    The HelloVote Providers adopt appropriate data collection, storage and processing practices and security measures to help protect against unauthorized access, alteration, disclosure or destruction of your personal information, but the HelloVote Providers cannot guarantee that your information is 100% secure.
 
-            <h2>How the HelloVote Providers Protect User Information</h2>
-            The HelloVote Providers adopt appropriate data collection, storage and processing practices and security measures to help protect against unauthorized access, alteration, disclosure or destruction of your personal information, but the HelloVote Providers cannot guarantee that your information is 100% secure.
+    <h2>Sharing Personal Information</h2>
+    <p>
+      The HelloVote Providers use third party service providers to help them operate the HelloVote Tool. These third party service providers have access to Users’ personally identifiable information, but must keep the information confidential and may use it only for purposes relating to the HelloVote Tool, and for no unrelated purpose. Data you send to HelloVote will be visible to your phone company (if you use SMS) or Facebook (if you use Facebook Messenger) and is protected according to their privacy policies and applicable law.
+    </p>
+    <p>
+      In addition, the HelloVote Providers will share a User’s name, mailing address, email address, date of birth, cell phone and political party preference (if required by a particular state and therefore provided by a User) with the organization that the User identifies as his or her source of referral to the HelloVote Tool (“Referring Organization”), if applicable. By identifying a Referring Organization to the HelloVote Providers, you consent to be contacted at that number on any subject from the Referring Organization, unless and until you request that such messages stop. The Referring Organization’s use of your personal information is otherwise governed by the privacy policy of such organization. If a User does not identify a third party organization as the source of referral, the HelloVote Providers will not share that User’s personal information with any third party, other than as set forth herein.
+    </p>
+    <p>
+      Notwithstanding the foregoing, the HelloVote Providers will share personally identifiable information about Users when required to do so by law, or in the good faith belief that such action is necessary to comply with state and federal laws or to respond to a court order, subpoena, or search warrant.
+    </p>
+    <p>
+      Moreover, the HelloVote Providers may share generic aggregated demographic information not linked to any personally identifiable information regarding Users with third parties.
+    </p>
 
-            <h2>Sharing Personal Information</h2>
-            <p>
-              The HelloVote Providers use third party service providers to help them operate the HelloVote Tool. These third party service providers have access to Users’ personally identifiable information, but must keep the information confidential and may use it only for purposes relating to the HelloVote Tool, and for no unrelated purpose. Data you send to HelloVote will be visible to your phone company (if you use SMS) or Facebook (if you use Facebook Messenger) and is protected according to their privacy policies and applicable law.
-            </p>
-            <p>
-              In addition, the HelloVote Providers will share a User’s name, mailing address, email address, date of birth, cell phone and political party preference (if required by a particular state and therefore provided by a User) with the organization that the User identifies as his or her source of referral to the HelloVote Tool (“Referring Organization”), if applicable. By identifying a Referring Organization to the HelloVote Providers, you consent to be contacted at that number on any subject from the Referring Organization, unless and until you request that such messages stop. The Referring Organization’s use of your personal information is otherwise governed by the privacy policy of such organization. If a User does not identify a third party organization as the source of referral, the HelloVote Providers will not share that User’s personal information with any third party, other than as set forth herein.
-            </p>
-            <p>
-              Notwithstanding the foregoing, the HelloVote Providers will share personally identifiable information about Users when required to do so by law, or in the good faith belief that such action is necessary to comply with state and federal laws or to respond to a court order, subpoena, or search warrant.
-            </p>
-            <p>
-              Moreover, the HelloVote Providers may share generic aggregated demographic information not linked to any personally identifiable information regarding Users with third parties.
-            </p>
+    <h2>Third Party Websites</h2>
+    <p>
+      The HelloVote Providers may provide links from the HelloVote Tool to other websites. The HelloVote Providers do not control the content or links that appear on these third party websites and is not responsible for the practices employed by websites linked to or from the HelloVote Tool. These websites and services have their own privacy policies. Browsing and interaction on any other website, including websites that have a link to or from the HelloVote Tool, is subject to that website’s own terms and policies. This Privacy Policy covers only information collected through the use of the HelloVote Tool. In addition, the HelloVote Providers do not currently respond to web browser “do not track” signals.
+    </p>
 
-            <h2>Third Party Websites</h2>
-            <p>
-              The HelloVote Providers may provide links from the HelloVote Tool to other websites. The HelloVote Providers do not control the content or links that appear on these third party websites and is not responsible for the practices employed by websites linked to or from the HelloVote Tool. These websites and services have their own privacy policies. Browsing and interaction on any other website, including websites that have a link to or from the HelloVote Tool, is subject to that website’s own terms and policies. This Privacy Policy covers only information collected through the use of the HelloVote Tool. In addition, the HelloVote Providers do not currently respond to web browser “do not track” signals.
-            </p>
+    <h2>Compliance with Children’s Online Privacy Protection Act</h2>
+    <p>
+      Protecting the privacy of the young is especially important. For that reason, the HelloVote Providers never collect information through the HelloVote Tool from individuals who the HelloVote Providers know are under 13, and no part of the HelloVote Tool is structured to attract anyone under 13. If the HelloVote Providers become aware of having information about anyone under 13, the HelloVote Providers will promptly delete the information.
+    </p>
 
-            <h2>Compliance with Children’s Online Privacy Protection Act</h2>
-            <p>
-              Protecting the privacy of the young is especially important. For that reason, the HelloVote Providers never collect information through the HelloVote Tool from individuals who the HelloVote Providers know are under 13, and no part of the HelloVote Tool is structured to attract anyone under 13. If the HelloVote Providers become aware of having information about anyone under 13, the HelloVote Providers will promptly delete the information.
-            </p>
+    <h2>California Privacy Rights</h2>
+    <p>
+      If you reside in California and have provided your personally identifiable information to the HelloVote Providers, you may request information once per calendar year about the HelloVote Providers’ disclosures of certain categories of your personally identifiable information to third parties for their direct marketing purposes. Such requests must be submitted to the HelloVote Providers at <a href="mailto:team@hello.vote">team@hello.vote</a>.
+    </p>
 
-            <h2>California Privacy Rights</h2>
-            <p>
-              If you reside in California and have provided your personally identifiable information to the HelloVote Providers, you may request information once per calendar year about the HelloVote Providers’ disclosures of certain categories of your personally identifiable information to third parties for their direct marketing purposes. Such requests must be submitted to the HelloVote Providers at <a href="mailto:team@hello.vote">team@hello.vote</a>.
-            </p>
+    <h2>Changes to this Privacy Policy</h2>
+    <p>
+      The HelloVote Providers may update this Privacy Policy at any time. When it does, the HelloVote Providers will revise the updated date at the bottom of this page. The HelloVote Providers encourage Users to frequently check this page for any changes to stay informed about what information the HelloVote Providers collect and how they use that information. You acknowledge and agree that it is you responsibility to review this Privacy Policy periodically and become aware of any modifications since your last visit.
+    <p>
 
-            <h2>Changes to this Privacy Policy</h2>
-            <p>
-              The HelloVote Providers may update this Privacy Policy at any time. When it does, the HelloVote Providers will revise the updated date at the bottom of this page. The HelloVote Providers encourage Users to frequently check this page for any changes to stay informed about what information the HelloVote Providers collect and how they use that information. You acknowledge and agree that it is you responsibility to review this Privacy Policy periodically and become aware of any modifications since your last visit.
-            <p>
+    <h2>Your Acceptance of These Terms</h2>
+    <p>
+      By using this the HelloVote Tool, you signify your acceptance of this Privacy Policy. If you do not agree to this policy, please do not use the HelloVote Tool. Your continued use of the HelloVote Tool following the posting of changes to this Privacy Policy will be deemed your acceptance of those changes.
+    </p>
 
-            <h2>Your Acceptance of These Terms</h2>
-            <p>
-              By using this the HelloVote Tool, you signify your acceptance of this Privacy Policy. If you do not agree to this policy, please do not use the HelloVote Tool. Your continued use of the HelloVote Tool following the posting of changes to this Privacy Policy will be deemed your acceptance of those changes.
-            </p>
+    <h2>Contacting the HelloVote Providers</h2>
+    <p>
+      If you have any questions about this Privacy Policy or anything relating to the HelloVote Tool, please contact the HelloVote Providers at <a href="mailto:team@hello.vote">team@hello.vote</a>. You may also call toll-free 844-344-3556. To stop receiving text messages from the HelloVote Providers, text “STOP”.
+    </p>
 
-            <h2>Contacting the HelloVote Providers</h2>
-            <p>
-              If you have any questions about this Privacy Policy or anything relating to the HelloVote Tool, please contact the HelloVote Providers at <a href="mailto:team@hello.vote">team@hello.vote</a>. You may also call toll-free 844-344-3556. To stop receiving text messages from the HelloVote Providers, text “STOP”.
-            </p>
-
-            <p>September 23, 2016</p>
-          </div> <!-- .c -->
-        </div> <!-- .row -->
-      </div> <!-- .wrapper -->
-    </section>
-
-    <SocialSidebar />
+    <p>September 23, 2016</p>
   </div>
 </template>
 
 <script>
-import config from '~/config'
-import { createMetaTags, smoothScrollToElement } from '~/assets/js/helpers'
-import Logo from '~/components/Logo'
-
 export default {
-  components: {
-    Logo
-  },
-
-  head() {
-    return {
-      title: config.sharing.title,
-      meta: createMetaTags({
-        title: config.sharing.title,
-        description: config.sharing.description,
-        image: config.sharing.image,
-        url: config.sharing.url
-      })
-    }
-  },
-
-  methods: {
-    scrollTo(hash) {
-      const duration = 500
-      smoothScrollToElement(hash, duration)
-      // WARNING: Since there is no server a setTimeout is ok. However, with a
-      // server this is a dangerous eval. Remove if this project ever is hosted
-      // with a JS server.
-      setTimeout(() => {
-        location.hash = hash
-      }, duration)
-    }
-  }
+  layout: 'article'
 }
 </script>

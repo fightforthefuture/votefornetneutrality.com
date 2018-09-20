566d95a09d4f7d1726837659a33a7c1b7a308ecc
Ken Mickles <ken@37i.net>
2018-09-20 17:47:16 -0400

    Use default OG tags on article pages

--- layouts/article.vue
+++ layouts/article.vue
@@ -17,11 +17,23 @@
 <script>
 import Logo from '~/components/Logo'
 import PageFooter from '~/components/PageFooter'
+import { createMetaTags } from '~/assets/js/helpers'
 
 export default {
   components: {
     Logo,
     PageFooter
-  }
+  },
+
+  head() {
+    return {
+      meta: createMetaTags({
+        title: config.sharing.title,
+        description: config.sharing.description,
+        image: config.sharing.image,
+        url: config.sharing.url
+      })
+    }
+  },
 }
 </script>

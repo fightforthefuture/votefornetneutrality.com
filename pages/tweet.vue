57b1237681f97aa97ad34ef8240ef70afc35adf9
Ken Mickles <ken@37i.net>
2018-09-21 13:38:42 -0400

    Add default tweet text + add /tweet redirect

---
+++ pages/tweet.vue
@@ -0,0 +1,38 @@
+<style lang="scss" scoped>
+section {
+  min-height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
+
+<template>
+  <section>
+    <div>
+      <Logo />
+      <br>
+      <SocialShareButtons />
+    </div>
+  </section>
+</template>
+
+<script>
+import config from '~/config'
+import Logo from '~/components/Logo'
+import SocialShareButtons from '~/components/SocialShareButtons'
+
+export default {
+  components: {
+    Logo,
+    SocialShareButtons
+  },
+
+  mounted() {
+    if (!process.server) {
+      const tweet = config.sharing.defaultTweetText.trim()
+      location.href = `https://twitter.com/intent/tweet?text=${encodeURIComponent(tweet)}`
+    }
+  }
+}
+</script>

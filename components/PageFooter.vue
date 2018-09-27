c180d6620e2b7afdd5bc7764876c34038fb8c607
shoshi <shoshiroberts@gmail.com>
2018-09-27 14:29:08 -0400

    Add Demand Progress

--- components/PageFooter.vue
+++ components/PageFooter.vue
@@ -2,11 +2,19 @@
   <div class="page-footer text-center">
     <div class="sml-pad-y4 logo-block">
       <div class="wrapper">
-        <p class="text-loud text-brand">Made by:</p>
-        <a href="https://www.fightforthefuture.org">
-          <img src="~assets/images/fftf-logo.svg" class="grid-center"
-               alt="Fight for the Future logo" />
-        </a>
+        <div class="sml-c12 lrg-c7 grid-center text-center">
+          <p class="text-loud text-brand">Made by:</p>
+          <div class="flex-row flex-center sml-push-y1">
+            <a href="https://www.fightforthefuture.org">
+              <img src="~assets/images/fftf-logo.svg" class="grid-center"
+                   alt="Fight for the Future logo" />
+            </a>
+            <a href="https://demandprogress.org/">
+              <img src="~assets/images/demand-progress-logo.svg" class="grid-center"
+                   alt="Demand Progress logo" />
+            </a>
+          </div> <!-- .flex-row -->
+        </div> <!-- .c -->
       </div> <!-- .wrapper -->
     </div> <!-- .pad -->
     <div class="contact-block sml-pad-y5">
@@ -19,6 +27,13 @@
             <a href="tel://9788526457">978-852-6457</a> or
             <a href="mailto:press@fightforthefuture.org">
               press@fightforthefuture.org</a>
+          </small>
+        </p>
+        <p class="tuck-bottom">
+          <small>
+            <a href="tel://2026817582">202-681-7582</a> or
+            <a href="mailto:press@demandprogress.org">
+              press@demandprogress.org</a>
           </small>
         </p>
         <p>

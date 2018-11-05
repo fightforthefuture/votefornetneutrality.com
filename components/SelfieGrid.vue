be65eb687a882d5ab8d74f5c345243873f822b59
shoshi <shoshiroberts@gmail.com>
2018-11-05 15:36:43 -0500

    New data set for selfies

--- components/SelfieGrid.vue
+++ components/SelfieGrid.vue
@@ -1,17 +1,18 @@
 <template>
   <div v-if="selfies">
     <div class="row sml-push-y1 med-push-y2">
-      <div v-for="(selfie, index) in selfies"
+      <div v-for="(selfie, index) in selfies.data"
            :key="`selfies-${index}`"
            class="sml-c3 lrg-c2 sml-push-y2">
-        <img :src="selfie.imageUrl" :alt="`Selfie ${index}`" />
+        <img :src="selfie.photo" :alt="`Selfie ${index}`" />
       </div> <!-- v-for -->
     </div> <!-- .row -->
 
     <div class="sml-push-y2 text-center">
       <a v-if="!isEndOfFeed" @click="loadMore" class="btn">Load More</a>
       <p v-else class="text-brand-light">
-        You&rsquo;ve reached the end of the feed
+        You&rsquo;ve reached the end of the feed, but there are still millions
+        more people fighting for Net Neutrality across the country.
       </p>
     </div> <!-- .push -->
   </div> <!-- v-if -->
@@ -22,23 +23,22 @@ export default {
   computed: {
     selfies() {
       if (this.$store.state.selfies) {
-        return this.$store.state.selfies.slice(0, this.curPageNum*this.perPageNum)
+        return this.$store.state.selfies
       }
     },
     isEndOfFeed() {
-      return this.curPageNum*this.perPageNum >= this.$store.state.selfies.length
+      return this.curPageNum >= this.$store.state.selfies.pages.page_count
     }
   },
 
   data () {
     return {
-      perPageNum: 24,
       curPageNum: 1
     }
   },
 
   mounted() {
-    this.$store.dispatch('getSelfies')
+    this.$store.dispatch('getSelfies', this.curPageNum)
 
     // `window` is not availabled in `created` lifecycle hook
     window.addEventListener('scroll', () => {
@@ -48,14 +48,15 @@ export default {
 
   methods: {
     loadMore() {
-      this.curPageNum++
+      if (!this.isEndOfFeed) {
+        this.curPageNum++
+        this.$store.dispatch('getSelfies', this.curPageNum)
+      }
     },
     bottomVisible() {
       const doc = document.documentElement
       let isBottomOfPage = doc.clientHeight + window.scrollY >= doc.scrollHeight
-      console.log('isBottomOfPage:', isBottomOfPage)
       if (isBottomOfPage) {
-        console.log('load more now...')
         this.loadMore()
       }
     }
